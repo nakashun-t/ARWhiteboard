3c45625a1aad50e4490a43e756bb625edfcea67d
ShuntaroNakajima <nakashun.t@gmail.com>
2020-04-02 23:13:40 +0900

    add generate whiteboard function

---
+++ Assets/Scripts/Whiteboard.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CasualMeeting
+{
+    public class Whiteboard : MonoBehaviour
+    {
+        private int whiteboardID;
+
+        public int WhiteboardID
+        {
+            set { this.whiteboardID = value; }
+            get { return whiteboardID; }
+        }
+
+        private void Start()
+        {
+            whiteboardID = GameObject.Find("WhiteboardManager").GetComponent<WhiteboardManager>().GeneratedWhiteboardNumber;
+            Debug.Log("WhiteboardID = " + whiteboardID);
+        }
+    }
+
+}
---
+++ Assets/Scripts/WhiteboardManager.cs
@@ -0,0 +1,50 @@
+﻿using Photon.Pun;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace CasualMeeting
+{
+    public class WhiteboardManager : MonoBehaviourPunCallbacks
+    {
+        private int localGenerateNumber = 0;
+        private int remoteGenerateNumber = 0;
+        private int generatedWhiteboardNumber = 0;
+
+        [SerializeField]
+        private GameObject whiteboardPrefab;
+
+        public int GeneratedWhiteboardNumber
+        {
+            get { return generatedWhiteboardNumber; }
+        }
+
+        private void Start()
+        {
+            GenerateWhiteboard();
+        }
+
+        public void GenerateWhiteboard()
+        {
+            GenerateLocal();
+
+            photonView.RPC(nameof(Generate), RpcTarget.Others, localGenerateNumber + remoteGenerateNumber);
+        }
+
+        private void GenerateLocal()
+        {
+            localGenerateNumber++;
+            generatedWhiteboardNumber = localGenerateNumber + remoteGenerateNumber;
+            Debug.Log("generatedNum = " + generatedWhiteboardNumber);
+            GameObject board = Instantiate(whiteboardPrefab, Vector3.zero, Quaternion.identity);
+        }
+
+        [PunRPC]
+        private void Generate(int num)
+        {
+            remoteGenerateNumber++;
+            generatedWhiteboardNumber = num;
+            Instantiate(whiteboardPrefab, Vector3.zero, Quaternion.identity);
+        }
+    }
+}
